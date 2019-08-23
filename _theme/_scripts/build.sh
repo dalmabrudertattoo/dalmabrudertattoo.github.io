016573503ba210af774c335b9f6d96c5667b4d9d
Barna Bruder <bruder.barna@gmail.com>
2019-08-23 18:15:57 +0200

    added font awesome

--- _theme/_scripts/build.sh
+++ _theme/_scripts/build.sh
@@ -9,8 +9,10 @@ cd "${0%/*}/.." || exit
 cp node_modules/jquery/dist/jquery.min.js public/js/
 cp node_modules/jquery.scrollto/jquery.scrollTo.min.js public/js/
 cp node_modules/lightgallery/dist/js/lightgallery.min.js public/js/
-cp node_modules/bootstrap/js/dist/modal.js public/js/
+cp node_modules/bootstrap/dist/js/bootstrap.min.js public/js/
 cp node_modules/lightgallery/dist/css/lightgallery.min.css public/css/
-cp node_modules/bootstrap/dist/bootstrap.min.css public/css/
+cp node_modules/bootstrap/dist/css/bootstrap.min.css public/css/
+cp node_modules/\@fortawesome/fontawesome-free/css/all.min.css public/css/fontawesome-all.min.css
 cp node_modules/lightgallery/dist/img/loading.gif public/img/
+cp -r node_modules/\@fortawesome/fontawesome-free/webfonts public/
 cp -r node_modules/lightgallery/dist/fonts public/
--- _theme/_scripts/clean.sh
+++ _theme/_scripts/clean.sh
@@ -5,8 +5,11 @@ cd "${0%/*}/.." || exit
 rm public/js/jquery.min.js
 rm public/js/jquery.scrollTo.min.js
 rm public/js/lightgallery.min.js
-rm public/js/modal.js
+rm public/js/bootstrap.min.js
+rm public/css/fontawesome-all.min.css
 rm public/css/lightgallery.min.css
 rm public/css/bootstrap.min.css
 rm public/img/loading.gif
+rm -rf public/webfonts
 rm -rf public/fonts
+
