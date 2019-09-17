2ae4989b308d11754afe6ba315311fee2110b2ab
Barna Bruder <bruder.barna@gmail.com>
2019-09-17 21:24:20 +0200

    change build to create files as running user

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -6,7 +6,7 @@ cd "${0%/*}/.." || exit
 
 _theme/_scripts/build.sh
 
-docker run --rm -v "$(pwd)":/work \
+docker run --rm -v "$(pwd)":/work -u $(id -u):$(id -g) \
   thumbsupgallery/thumbsup \
     thumbsup \
       --input /work/_input \
